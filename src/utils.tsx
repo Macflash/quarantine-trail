d2195a6b403265d7861e94e6d38e58c7b4f149b9
Macflash <the.alex.breyfogle@gmail.com>
2020-03-30 23:02:51 -0700

    format

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 
 export function InRange(low: number, high: number): number {
--- src/views/city.tsx
+++ src/views/city.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export interface City {
-    day: number;
-    name: string;
-    population: number;
-    infected: number;
-    recovered: number;
-    interaction: number; // estimated numbers out each day, which will determine spread speed
-    protection: number; // some scale for gloves and masks etc
+  day: number;
+  name: string;
+  population: number;
+  infected: number;
+  recovered: number;
+  interaction: number; // estimated numbers out each day, which will determine spread speed
+  protection: number; // some scale for gloves and masks etc
 
-    testedPositive: number;
-    deceased: number;
+  testedPositive: number;
+  deceased: number;
 }
 
 export const CityView: React.FC<{ c: City }> = props => {
