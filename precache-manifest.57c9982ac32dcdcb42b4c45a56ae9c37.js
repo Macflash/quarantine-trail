6f033ac8d017f5b4ffc7cd821c3739da72dc0035
Macflash <the.alex.breyfogle@gmail.com>
2020-04-17 09:23:00 -0700

    Updates

---
+++ precache-manifest.57c9982ac32dcdcb42b4c45a56ae9c37.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4cde56652864369c134b7f37e41b2d",
+    "url": "/github.io/quarantine-trail/index.html"
+  },
+  {
+    "revision": "44919c5716f126ca0874",
+    "url": "/github.io/quarantine-trail/static/css/main.37274586.chunk.css"
+  },
+  {
+    "revision": "a72d62c470db9f18e1a1",
+    "url": "/github.io/quarantine-trail/static/js/2.b4a57d6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/quarantine-trail/static/js/2.b4a57d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44919c5716f126ca0874",
+    "url": "/github.io/quarantine-trail/static/js/main.6e297d35.chunk.js"
+  },
+  {
+    "revision": "96bdaf3ed7c6523e3c59",
+    "url": "/github.io/quarantine-trail/static/js/runtime-main.f162d369.js"
+  },
+  {
+    "revision": "3685d7185b5567941a3187c17501f11d",
+    "url": "/github.io/quarantine-trail/static/media/QT Intro Theme.3685d718.wav"
+  },
+  {
+    "revision": "200e8af1c550bb0d3cf9ac7bed1ff894",
+    "url": "/github.io/quarantine-trail/static/media/cleanbad.200e8af1.png"
+  },
+  {
+    "revision": "07c0ba586327fad6073f993da737ea47",
+    "url": "/github.io/quarantine-trail/static/media/cleangreat.07c0ba58.png"
+  },
+  {
+    "revision": "514497c21d1edcf575f59661115da346",
+    "url": "/github.io/quarantine-trail/static/media/cleanok.514497c2.png"
+  },
+  {
+    "revision": "e774b7c6518c2931c09ed98f6bc84a98",
+    "url": "/github.io/quarantine-trail/static/media/graph.e774b7c6.png"
+  },
+  {
+    "revision": "392a8b162be6a383a394438074654a2b",
+    "url": "/github.io/quarantine-trail/static/media/help.392a8b16.png"
+  },
+  {
+    "revision": "2df3933a6e8b2b395b6a40366a1b7221",
+    "url": "/github.io/quarantine-trail/static/media/longshift.2df3933a.png"
+  },
+  {
+    "revision": "fd298798b5683751ebf57e3081de6142",
+    "url": "/github.io/quarantine-trail/static/media/paybad.fd298798.png"
+  },
+  {
+    "revision": "16d28253314830a21b6b93f0e83050cb",
+    "url": "/github.io/quarantine-trail/static/media/regularshift.16d28253.png"
+  },
+  {
+    "revision": "eafff0a1383d1f986f0e9f22789c6d35",
+    "url": "/github.io/quarantine-trail/static/media/shortshift.eafff0a1.png"
+  },
+  {
+    "revision": "1521c45c28c7a6f589751318550237aa",
+    "url": "/github.io/quarantine-trail/static/media/supplies.1521c45c.png"
+  },
+  {
+    "revision": "89a40b1c3c0671a11ee8155b2040ab3f",
+    "url": "/github.io/quarantine-trail/static/media/virus1.89a40b1c.bmp"
+  },
+  {
+    "revision": "e6de495fb2060c0372f6a13010b53908",
+    "url": "/github.io/quarantine-trail/static/media/virus2.e6de495f.bmp"
+  },
+  {
+    "revision": "366f569bf950477905badf925cabae2b",
+    "url": "/github.io/quarantine-trail/static/media/virus3.366f569b.bmp"
+  }
+]);
